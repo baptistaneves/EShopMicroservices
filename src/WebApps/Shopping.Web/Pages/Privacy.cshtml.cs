c9fd4a9570af78813ebb33c43cb687322d44ce87
Baptista Neves <baptistafirminoneves@gmail.com>
2024-08-08 13:08:34 +0100

    Add WebApp Razor Pages

---
+++ src/WebApps/Shopping.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Pages;
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
